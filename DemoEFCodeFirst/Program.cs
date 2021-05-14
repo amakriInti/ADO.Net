c1384bddcd657800d986a170ab792211027b228a
Ali MAKRI <microsoft@makri.fr>
2021-05-14 11:44:07 +0200

    11:43

---
+++ DemoEFCodeFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEFCodeFirst
+{
+    // https://www.entityframeworktutorial.net/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ExoCFContext();
+            var articles = context.Articles.ToList();
+            Console.WriteLine("C'est fait !");
+            Console.ReadLine();
+        }
+    }
+    class ExoCFContext : DbContext
+    {
+        public ExoCFContext() : base("name=ExoCFConfig") { }
+        public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<Stock> Stocks { get; set; }
+
+    }
+    class Article
+    {
+        public Guid Id { get; set; }
+        public string Nom { get; set; }
+        public decimal Prix { get; set; }
+    }
+    class Stock
+    {
+        public Guid Id { get; set; }
+        public Guid Article { get; set; }
+        public int Qte { get; set; }
+
+    }
+}
