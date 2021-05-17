5bbb29cc4bf84e9529789cb6a8770f8b9ac3c79e
Ali MAKRI <microsoft@makri.fr>
2021-05-17 10:10:38 +0200

    10:10

---
+++ TPAdo.Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPAdo.Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0. Mode connecté
+
+            // 1. Nouvelle gouvernante affectée à l'hôtel
+
+            // 2. Nouveau réceptionniste de l'hôtel
+
+            // 3. Nouvel Hôtel
+
+            // 4. 2 nouvelles chambres 101 102 dans l'hôtel à l'étage 1
+
+            // 5. 2 femmes de chambre A et B affectées à l'hôtel
+
+            // 6. 1 intervention : A fait la 101 - B fait la 102
+
+            Console.ReadLine();
+        }
+    }
+}
