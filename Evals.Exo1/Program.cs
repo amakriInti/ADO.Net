bafd2b3c6d2bf873d118e150ad182251b291cfd1
Ali MAKRI <microsoft@makri.fr>
2021-05-19 09:32:06 +0200

    09:31

--- Evals.Exo1/Program.cs
+++ Evals.Exo1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,31 @@ namespace Evals.Exo1
 {
     class Program
     {
+        private static string Requete = @"SELECT Production.ProductCategory.Name nom, COUNT(Production.Product.ProductID) n
+                                    FROM Production.Product INNER JOIN
+                                             Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN
+                                             Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID
+                                    GROUP BY Production.ProductCategory.Name
+                                    ORDER BY Production.ProductCategory.Name";
         static void Main(string[] args)
         {
+            var cnx = new SqlConnection();
+            cnx.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks2016;Integrated Security=True";
+            cnx.Open();
+
+            var cmd = new SqlCommand();
+            cmd.Connection = cnx;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = Requete;
+
+            var rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                Console.WriteLine("{0} : {1}", rd["nom"], rd["n"]);
+            }
+            rd.Close();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Evals.Exo2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evals.Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AdvContext();
+            var liste = context.People
+                .Where(p => p.BusinessEntity.BusinessEntityAddresses.Count() > 0)
+                .Select(p => new
+                {
+                    Ville = p.BusinessEntity.BusinessEntityAddresses.FirstOrDefault().Address.City,
+                    Nom = p.LastName,
+                    Prenom = p.FirstName
+                })
+                .OrderBy(p => p.Ville)
+                .ToList();
+
+            string ville = "";
+            foreach (var personne in liste)
+            {
+                if (personne.Ville != ville)
+                {
+                    ville = personne.Ville;
+                    Console.WriteLine(ville);
+                }
+                Console.WriteLine("\t{0} - {1}",
+                    personne.Nom,
+                    personne.Prenom);
+            }
+            Console.ReadLine();
+        }
+    }
+}
